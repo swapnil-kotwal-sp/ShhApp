3a1f7d1c817570444ffc5ff9b836f83b9a0ea5b3
Swapnil Kotwal <swapnil@vertisinfotech.com>
2013-11-11 13:44:59 +0530

    added umangs commit again for resolving conflict

--- gen/com/example/shhapp/R.java
+++ gen/com/example/shhapp/R.java
@@ -25,29 +25,24 @@ public final class R {
         public static final int back=0x7f070000;
     }
     public static final class drawable {
-        public static final int custom_btn=0x7f020000;
-        public static final int green_grass=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int shh=0x7f020003;
+        public static final int green_grass=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int shh=0x7f020002;
     }
     public static final class id {
-        public static final int Button01=0x7f0a000f;
-        public static final int Spinner01=0x7f0a0004;
-        public static final int btnDecrypt=0x7f0a0007;
-        public static final int btnEmail=0x7f0a000d;
-        public static final int btnEncrptSms=0x7f0a0006;
-        public static final int btnReadEmail=0x7f0a000c;
-        public static final int btnReadSms=0x7f0a0009;
-        public static final int btnSendSms=0x7f0a000a;
+        public static final int Button01=0x7f0a000b;
+        public static final int Spinner01=0x7f0a0009;
+        public static final int btnDecrypt=0x7f0a0002;
+        public static final int btnEmail=0x7f0a0004;
+        public static final int btnEncrptSms=0x7f0a0003;
+        public static final int btnReadEmail=0x7f0a0006;
+        public static final int btnReadSms=0x7f0a0007;
+        public static final int btnSendSms=0x7f0a0005;
         public static final int linearLayout1=0x7f0a0000;
-        public static final int menu_settings=0x7f0a0011;
-        public static final int password=0x7f0a0010;
-        public static final int spinner=0x7f0a0003;
-        public static final int tableRow1=0x7f0a0002;
-        public static final int tableRow2=0x7f0a0005;
-        public static final int tableRow3=0x7f0a0008;
-        public static final int tableRow4=0x7f0a000b;
-        public static final int username=0x7f0a000e;
+        public static final int menu_settings=0x7f0a000d;
+        public static final int password=0x7f0a000c;
+        public static final int spinner=0x7f0a0008;
+        public static final int username=0x7f0a000a;
         public static final int viewSmsText=0x7f0a0001;
     }
     public static final class layout {
@@ -103,8 +98,5 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080001;
-        /**  Button Style 
-         */
-        public static final int mybuttonstyle=0x7f080002;
     }
 }
